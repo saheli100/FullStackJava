cc5e49b75afe40e7122d8f81f0376ffd2b91e648
saheli100 <131791899+saheli100@users.noreply.github.com>
2023-06-12 13:22:00 +0530

    Add files via upload

---
+++ Microservice/3/customer-service/target/classes/data.sql
@@ -0,0 +1,2 @@
+insert into customer_table(customer_id,email,first_name,last_name) values(100,'john@gmail.com','john','doe');
+insert into customer_table(customer_id,email,first_name,last_name) values(101,'marry@gmail.com','marry','kom');
